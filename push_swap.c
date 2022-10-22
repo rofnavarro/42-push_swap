34202062f671fb0db36ca802e1b667eba6e5fc47
rofnavarro <rofnavarro@gmail.com>
2022-10-21 21:54:32 -0300

    push_swap_v4

--- push_swap.c
+++ push_swap.c
@@ -6,25 +6,54 @@
 /*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/29 23:21:18 by rferrero          #+#    #+#             */
-/*   Updated: 2022/10/17 12:51:35 by rferrero         ###   ########.fr       */
+/*   Updated: 2022/10/21 21:52:59 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static void check_args(int argc, char *argv[]);
+static void	check_args(int argc, char *argv[]);
+static void	check_type(int argc, char *argv[]);
 
 int	main(int argc, char *argv[])
 {
 	check_args(argc, argv);
+	check_type(argc, argv);
 	return (0);
 }
 
-static void check_args(int argc, char *argv[])
+static void	check_args(int argc, char *argv[])
 {
-	if(!argc || argc < 3)
+	int	i;
+
+	i = 0;
+	if (!argc || argc < 3)
 	{
-		ft_printf("Error!\nWrong number of arguments!\n");
-		exit(1);
+		if (argc == 1)
+			exit(EXIT_FAILURE);
+		ft_printf("Error!\n");
+		exit(EXIT_FAILURE);
+	}
+}
+
+static void	check_type(int argc, char *argv[])
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	j = 0;
+	while (argv[i])
+	{
+		while (argv[i][j])
+		{
+			if (argv[i][0] != '-' && ft_isdigit(argv[i][j]) == 0)
+			{
+				ft_printf("Error!\n array\n");
+				exit(EXIT_FAILURE);
+			}
+			j++;
+		}
+		i++;
 	}
 }
