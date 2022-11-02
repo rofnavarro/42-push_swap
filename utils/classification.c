9b0c3d54a0f72747c455e0b73870edb7153d915a
rofnavarro <rofnavarro@gmail.com>
2022-11-02 19:01:32 -0300

    push_swap_v15

--- utils/classification.c
+++ utils/classification.c
@@ -1,29 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   push_swap.c                                        :+:      :+:    :+:   */
+/*   classification.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/09/29 23:21:18 by rferrero          #+#    #+#             */
-/*   Updated: 2022/11/02 19:00:49 by rferrero         ###   ########.fr       */
+/*   Created: 2022/11/02 19:01:07 by rferrero          #+#    #+#             */
+/*   Updated: 2022/11/02 19:01:16 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "push_swap.h"
-
-int	main(int argc, char *argv[])
-{
-	t_node *pile_a;
-
-	pile_a = NULL;
-	ft_check(argc, argv);
-	ft_run(pile_a, argc, argv);
-
-	ft_classification();
-
-	ft_printf("programa rodando\n");
-	
-	
-	return (0);
-}
+#include "../push_swap.h"
