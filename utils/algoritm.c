8d8d7072629e56cf77e6561d5ed0f6d89cde1121
rofnavarro <rofnavarro@gmail.com>
2022-11-25 18:59:32 -0300

    push_swap_v32

--- utils/algoritm.c
+++ utils/algoritm.c
@@ -6,7 +6,7 @@
 /*   By: rferrero <rferrero@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 13:45:41 by rferrero          #+#    #+#             */
-/*   Updated: 2022/11/25 18:46:41 by rferrero         ###   ########.fr       */
+/*   Updated: 2022/11/25 18:57:30 by rferrero         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,13 @@ void	ft_algoritm(t_piles *piles)
 {
 	int	i;
 
-	i = ft_mid_value(&piles->stack_a);
+	i = 0;
 	while (i < piles->stack_a.size)
 	{
 		ft_printf("%d\n", piles->stack_a.stack[i]);
 		i++;
 	}
-	ft_printf("%d ", i);
+	i = ft_mid_value(&piles->stack_a);
+	ft_printf("\n\n%d ", i);
 }
 
